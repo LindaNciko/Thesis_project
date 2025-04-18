fbd7038238d842424bfabc975ca5961dd8e79875
LindaNciko <lindawanciko20@gmail.com>
2025-04-18 11:42:36 +0300

    force push to update

---
+++ test_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Streamlit Deployment Test")
+st.write("If you're seeing this, the app is working!")
+st.write("This is a test to ensure that the Streamlit app is deployed correctly.")
